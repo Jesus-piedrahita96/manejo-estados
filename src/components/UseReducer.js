3013790c3467aedcbacd2f949d93ebdb4e2e6e56
Jesus-piedrahita96 <jesus-piedrahitakali-jesus.Jesus-piedrahita96>
2023-01-18 11:59:13 -0500

    manejo de los actionTypes con useReducer

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -15,10 +15,10 @@ function UseReducer(props) {
     if (estado.loading) {
       setTimeout(() => {
         if (estado.valor === SECURITY_CODE) {
-          dispatch({type: 'CONFIRM'})
+          dispatch({type: actionTypes.confirm})
 
         } else {
-          dispatch({type: 'ERROR'})
+          dispatch({type: actionTypes.error})
         }
       }, 3000);
     }
@@ -47,11 +47,11 @@ function UseReducer(props) {
           id="buscador"
           value={estado.valor}
           onChange={(event) => {
-            dispatch({type: 'CHANGE', payload: event.target.value})
+            dispatch({type: actionTypes.change, payload: event.target.value})
           }}
         />
         <button
-          onClick={() => {dispatch({type: 'RECARGAR'})}}
+          onClick={() => {dispatch({type: actionTypes.recargar})}}
         >
           Comprobar
         </button>
@@ -62,15 +62,15 @@ function UseReducer(props) {
       <div>
         <h2>Eliminar {props.name}</h2>
         <p>Seguro que quieres eliminar {props.name}?</p>
-        <button onClick={() => {dispatch({type: 'ELIMINAR'})}}>Si, eliminar</button>
-        <button onClick={() => {dispatch({type: 'VOLVER'})}}>No, volver</button>
+        <button onClick={() => {dispatch({type: actionTypes.eliminar})}}>Si, eliminar</button>
+        <button onClick={() => {dispatch({type: actionTypes.volver})}}>No, volver</button>
       </div>
     )
   } else {
     return (
       <div>
         <p>Eliminado con exito</p>
-        <button onClick={() => {dispatch({type: 'RESETEAR'})}}>Resetear</button>
+        <button onClick={() => {dispatch({type: actionTypes.resetear})}}>Resetear</button>
       </div>
     )
   }
@@ -84,36 +84,46 @@ const initialState = {
   confirmed: false
 }
 
+const actionTypes = {
+  confirm: 'CONFIRM',
+  error: 'ERROR',
+  change: 'CHANGE',
+  recargar: 'RECARGAR',
+  eliminar: 'ELIMINAR',
+  volver: 'VOLVER',
+  resetear: 'RESETEAR'
+}
+
 const reducerObject = (state, payload) => ({
-  'CONFIRM': {
+  [actionTypes.confirm]: {
     ...state,
     loading: false,
     confirmed: true
   },
-  'ERROR': {
+  [actionTypes.error]: {
     ...state,
     error: true,
     loading: false
   },
-  'CHANGE': {
+  [actionTypes.change]: {
     ...state,
     valor: payload
   },
-  'RECARGAR': {
+  [actionTypes.recargar]: {
     ...state,
     loading: true,
     error: false
   },
-  'ELIMINAR': {
+  [actionTypes.eliminar]: {
     ...state,
     deleted: true
   },
-  'VOLVER': {
+  [actionTypes.volver]: {
     ...state,
     confirmed: false,
     valor: ''
   },
-  'RESETEAR':{
+  [actionTypes.resetear]:{
     ...state,
     confirmed: false,
     deleted: false,
